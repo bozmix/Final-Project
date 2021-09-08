c40da06bad4569896e1d4907f67e06e9237cb118
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-09-08 22:31:11 +0200

    Header completed, components folders and subfolders opened

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+
+export const Header = () => {
+    return (
+        <header className="p-5">
+            <nav className="navbar bg-light fixed-top">
+                <div className="container-fluid">
+                    <Link className="interviewsReports" to="/home">
+                        <h1 className="text-dark ms-5">
+                            Interviews Reports
+                        </h1></Link>
+                    <button type="button" className="btn btn-light btn-outline-secondary me-5 ps-5 pe-5">
+                        Candidates
+                    </button>
+                </div>
+            </nav>
+        </header>
+    );
+};
