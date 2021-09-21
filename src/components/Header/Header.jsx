c6356f0056041dfd9371260114a946494df9f628
Svemiliva <mshtatkich@gmail.com>
2021-09-21 15:44:34 +0200

    header colapse

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,30 +11,37 @@ export const Header = ({ changeLogIn }) => {
 
 
     return (
-        <header className="bg-info">
-            <nav className="navbar bg-info navbar-expand-sm fixed-top">
+        <header className="container ">
+            <nav className="navbar bg-info navbar-expand-lg fw-bold fixed-top">
 
-                <Link className="interviewsReports col-lg-6" to="/home">
-                    <h1 className="text-dark ms-5 me-5">
+                <Link className="interviewsReports ms-5" to="/home">
+                    <h1 className="text-dark">
                         Interviews Reports
                     </h1>
                 </Link>
-                <div className="pagesButtons col-xs-6 col-sm-6 col-lg-6 text-center mx-auto">
+                <button className="navbar-toggler font-weight-bold rounded me-sm-5"
+                    type="button">
+                    <i className="fas fa-bars"></i>
+                </button>
+
+                <div className="pagesButtons collapse navbar-collapse justify-content-lg-end">
+                <div className="navbar-nav ml-auto"> 
                     <Link className="candidatesLinks" to="/home">
-                        <button type="button" className="btnCandidates btn btn-light btn-outline-secondary m-2">
+                        <button type="button" className="btnCandidates btn btn-light btn-outline-secondary nav-item mx-0 mx-lg-1">
                             Candidates
                         </button>
                     </Link>
 
                     <Link className="reportsLinks" to="/reports">
-                        <button type="button" className="btnReports btn btn-light btn-outline-secondary m-2">
+                        <button type="button" className="btnReports btn btn-light btn-outline-secondary nav-item mx-0 mx-lg-1">
                             Reports
                         </button>
                     </Link>
 
-                    <button type="button" onClick={logOut} className="btnLogOut btn btn-light btn-outline-secondary m-2">
+                    <button type="button" onClick={logOut} className="btnLogOut btn btn-light btn-outline-secondary nav-item mx-0 me-lg-5">
                         Log Out
                     </button>
+                </div>
                 </div>
             </nav>
         </header>
